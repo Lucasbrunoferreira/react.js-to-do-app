0fce0040539b7f4de131f9c13ee5a9332735b494
Lucas Bruno <lucasbrunoferreira@gmail.com>
2019-02-27 22:10:17 -0300

    Struturing boilerpate of application

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../styles/pages/home.scss'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <span>Home</span>
+      </div>
+    )
+  }
+}
